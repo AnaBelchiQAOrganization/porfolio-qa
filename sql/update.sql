050d1e9d40485a5992c493acd619a3ff93e36508
Ana Belchí <ana@anabelchi.com>
2026-05-14 17:53:50 +0200

    Update update.sql
    añadidas actualizaciones de datos (update y delete)

---
+++ sql/update.sql
@@ -0,0 +1,15 @@
+-- Cambiar email de Isabel por isabelcurso@gmail.com
+update formanabelchi
+set email = 'isabelcurso@gmail.com'
+where name = 'Isabel';
+
+-- Cambiar edad 20 por 21
+update formanabelchi
+set age = 21 
+where age = 22
+
+-- Eliminar todos los registros con edad 30
+delete from formanabelchi where age = 30;
+
+-- Eliminar todos los registros con nombre Pedro
+delete from formanabelchi where name = 'Pedro';
