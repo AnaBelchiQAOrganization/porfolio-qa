c862ecd17cb2288c5f18effedbc281e6887c14aa
Ana Belchí <ana@anabelchi.com>
2026-05-17 11:37:19 +0200

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -1,12 +1,12 @@
 -- Cambiar email de Isabel por isabelcurso@gmail.com
 update formanabelchi
-set email = 'isabelcurso@gmail.com'
-where name = 'Isabel';
+   set email = 'isabelcurso@gmail.com'
+ where name = 'Isabel';
 
 -- Cambiar edad 20 por 21
 update formanabelchi
-set age = 21 
-where age = 22
+   set age = 21 
+ where age = 22
 
 -- Eliminar todos los registros con edad 30
 delete from formanabelchi where age = 30;
