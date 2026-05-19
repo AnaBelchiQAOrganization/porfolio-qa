e936819d14bfdfb714d28c604896d0984dc23cbe
Ana Belchí <ana@anabelchi.com>
2026-05-19 20:13:07 +0200

    Update delete.sql

---
+++ sql/delete.sql
@@ -0,0 +1,5 @@
+-- Eliminar todos los registros con edad 30
+delete from formanabelchi where age = 30;
+
+-- Eliminar todos los registros con nombre Pedro
+delete from formanabelchi where name = 'Pedro';
