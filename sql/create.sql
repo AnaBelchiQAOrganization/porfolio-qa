66f2c41e7709f6153eb68559d5135a584af0e33e
Ana Belchí <ana@anabelchi.com>
2026-05-19 19:39:34 +0200

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -2,7 +2,7 @@ create table formanabelchi (
   id serial primary key,
   name varchar(100),
   email varchar(100) not null,
-  subject varchar(25) not null
-  age  integer check (age>18),
   message varchar(500) not null,
+  age  integer check (age>18),
+  subject varchar(25) not null
   );
