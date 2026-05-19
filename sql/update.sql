fc78ef1b036dfabc336eea4215cfc651eb95a7e8
Ana Belchí <ana@anabelchi.com>
2026-05-19 20:13:29 +0200

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -7,9 +7,3 @@ update formanabelchi
 update formanabelchi
    set age = 21 
  where age = 22;
-
--- Eliminar todos los registros con edad 30
-delete from formanabelchi where age = 30;
-
--- Eliminar todos los registros con nombre Pedro
-delete from formanabelchi where name = 'Pedro';
